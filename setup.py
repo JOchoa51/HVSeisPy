ce2e6b4151fdb7b8598c96d72e501e9a4c4d543e
JOchoa51 <100816428+JOchoa51@users.noreply.github.com>
2025-02-18 11:38:16 -0600

    Update setup.py
    Added project information

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup, find_packages
 setup(
     name="hvseispy",  # Package name
     version="1.0",  # Version number
-    author="Your Name",
-    author_email="your.email@example.com",
-    description="A short description of your project",
+    author="Jesús Ochoa",
+    author_email="ochoacontrerasjesus8@gmail.com",
+    description="HVSeisPy calculates HV spectral ratio from 3-channel acceleration data, as well as various signal analysis operations.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/yourusername/my_project",  # GitHub or project URL
+    url="https://github.com/JOchoa51/HVSeisPy",  # GitHub or project URL
     packages=find_packages(),  # Automatically find package directories
     classifiers=[  # Metadata for PyPI
         "Programming Language :: Python :: 3",
@@ -18,6 +18,9 @@ setup(
     python_requires=">=3.6",  # Minimum Python version
     install_requires=[  # Dependencies
         "numpy",
-        "pandas",
+        "scipy",
+        "obspy",
+        "matplotlib",
+        "pykooh"
     ],
 )
