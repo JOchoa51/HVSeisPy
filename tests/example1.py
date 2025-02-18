933197bd5e886ea9d68b99e6fa02c9631a96bb5a
JOchoa51 <100816428+JOchoa51@users.noreply.github.com>
2025-02-18 17:18:19 -0600

    Update example1.py
    Added more examples for different file formats

--- tests/example1.py
+++ tests/example1.py
@@ -1,30 +1,33 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from hvseispy import seismic, plot_tools
+from hvseispy import seismic, plot_tools, IO
 
-# Se lee el archivo con datos de los 3 canales.
-# `data` contiene 3 vectores con los datos de aceleración de cada canal
-# ! IMPORTANTE: SpectraPy asume que los canales están en orden: [N, Z, E]
+# Read the file with data from the 3 channels.
+# `data` contains 3 vectors with the acceleration data of each channel
+# ! IMPORTANT: SpectraPy assumes that the channels are in order: [N, Z, E]
 
-data = np.loadtxt(r'hvseispy\data\Sample data\CO5620170908044918_nve100.txt', unpack=True)
+# data = np.loadtxt(r'hvseispy\data\Sample data\CO5620170908044918_nve100.txt', unpack=True)
 
+data = IO.read_sac([r"hvseispy\data\Sample data\KO.KHMN..HNN.D.2023.037.064738.SAC",r"hvseispy\data\Sample data\KO.KHMN..HNZ.D.2023.037.064738.SAC",r"hvseispy\data\Sample data\KO.KHMN..HNE.D.2023.037.064738.SAC"])
+
+# data = IO.read_mseed(r"hvseispy\data\Sample data\TK3126_100.mseed")
 hvm, hv, freq = seismic.hvsr(acc_data=data,
-							dt=0.01, # inverso de la tasa de muestreo
-							win_len=81.92, # longitud de la ventana de analisis en segundos
-							taper=['cosine', 0.15], # ventana de taper y porcentaje de aplicación (ver documentacion de la funcion para todas las ventanas aceptadas)
-							smooth_bandwidth=40, # constante de la ventana de suavizamiento konno-ohmachi
-							overlap=0, # traslape entre ventanas 
-							fftmin=0.1, # frecuencia minima de calculo de la FFT
-							fftmax=50, # frecuencia minima de calculo de la FFT (frecuencia de Nyquist o menor)
-							hvmin=0.1, # frecuencia minima de calculo del espectro HV
-							hvmax=50) # frecuencia minima de calculo del espectro HV (frecuencia de Nyquist o menor)
+                            dt=0.01, # inverse of the sampling rate
+                            win_len=30, # length of the analysis window in seconds
+                            taper=['cosine', 0.15], # taper window and application percentage (see function documentation for all accepted windows)
+                            smooth_bandwidth=40, # constant of the konno-ohmachi smoothing window
+                            overlap=0, # overlap between windows 
+                            fftmin=0.1, # minimum frequency for FFT calculation
+                            fftmax=50, # maximum frequency for FFT calculation (Nyquist frequency or lower)
+                            hvmin=0.1, # minimum frequency for HV spectrum calculation
+                            hvmax=50) # maximum frequency for HV spectrum calculation (Nyquist frequency or lower)
 
-PlotTools.plot_hv(hvm, hv, freq,
-				  fmin=0.1, # limite inferior de la grafica
-				  fmax=50, # limite superior de la grafica
-				  name='DM12', # nombre de la estacion o identificador del espectro
-				  plot_windows=True, # dibujar o no todas las ventanas (si es False, solamente grafica la ventana promedio)
-				  period_or_freq='freq' # graficar las curvas en eje de frecuencias o de periodos
-				  ) 
+plot_tools.plot_hv(hvm, hv, freq,
+                  fmin=0.1, # lower limit of the plot
+                  fmax=50, # upper limit of the plot
+                  name='station', # station name or spectrum identifier
+                  plot_windows=True, # whether to draw all windows (if False, only plots the average window)
+                  period_or_freq='freq', # plot the curves on a frequency or period axis
+                  ) 
 
 plt.show()
